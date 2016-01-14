d40101cebdad9225524140d9c3a8bc2341532d27
Carl Weis <carl@carlweis.com>
2016-01-14 03:24:20 -0600

    All models defined

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,9 @@ class User < ActiveRecord::Base
   has_many :text_posts, dependent: :destroy
   has_many :image_posts, dependent: :destroy
 
+  # comments
+  has_many :comments
+
   def following?(leader)
     leaders.include? leader
   end
