bfc6095816bb3f4eaa83c68b7b248d38f65e37c6
Carl Weis <carl@carlweis.com>
2016-01-14 06:58:23 -0600

    Add user timeline posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,9 @@ class ApplicationController < ActionController::Base
       @current_user ||= User.find(session[:user_id])
     end
   end
-
   helper_method :current_user
+
+  def authenticate_user!
+    redirect_to login_path unless current_user
+  end
 end
