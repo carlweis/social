01e3b63a6b1bc27541b9f79c8c169377f2dcbd4d
Carl Weis <carl@carlweis.com>
2016-01-14 08:07:01 -0600

    Write tests to back signup, signin, browse and logout

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get new with new user' do
+    get :new
+
+    user = assigns(:user)
+
+    assert user.new_record?
+    assert_response :success
+  end
+
+  test 'should create user' do
+    params = {
+      user: {
+        email: 'user@example.com',
+        password: 'password',
+        password_confirmation: 'password'
+      }
+    }
+
+    post :create, params
+
+    assert_redirected_to root_url
+  end
+
+  test 'should not create user' do
+    params = {
+      user: {
+        email: 'user@example.com',
+        password: 'passwo',
+        password_confirmation: 'password'
+      }
+    }
+
+    post :create, params
+
+    assert_template 'new'
+  end
+
 end
